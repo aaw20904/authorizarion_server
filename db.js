0ef886a9067845721a8e890a6823819f6279896d
aaw20904 <kozakizdona@gmail.com>
2023-07-29 15:57:24 +0300

    6

--- db.js
+++ db.js
@@ -62,14 +62,23 @@ class MysqlLayer {
     async closeDatabase(){
         return await this.#bdPool.end();
      }
-
+    ///***************OK! tested!! */
      async createNewUser (par={name:"",password:"", email:"example@mail.com", picture:"123", passw:0, salt:0, phone:"911"}) {
         let connection, generated_identifier;
          
             connection = await this.#bdPool.getConnection();
             await connection.beginTransaction();
             try{
-                generated_identifier = await  connection.query('INSERT INTO user_mail ( email) VALUES (?)', [par.email])
+             //firstly fill a user_mail table:
+                generated_identifier = await  connection.query('INSERT INTO user_mail ( email) VALUES (?)', [par.email]);
+                //get generated Id by system
+                generated_identifier = generated_identifier[0].insertId;
+                //write user info using gnerated by MySQL user_id:
+                await connection.query('INSERT INTO users ( user_id, passw, picture, uname,  salt, phone) VALUES (?, ?, ?, ?, ?, ?)',
+                                          [generated_identifier, par.password, par.picture, par.name, par.salt, par.phone]);
+                //apply 
+                await connection.commit();
+                
             }catch(e){
                //has a user already exists?
                 if(err.errno === 1062){
@@ -77,125 +86,15 @@ class MysqlLayer {
                 } else{
                     err.alrEx =false;
                 }
+                await connection.rollback();
                 throw new Error(e);
+            }finally{
+                connection.release();
             }
-            //get generated Id by system
-            generated_identifier = generated_identifier[0].insertId;
-
-            
-       
-        
   
      }
 
-    async createNewUser_old(par={name:"",password:"", email:"example@mail.com", picture:"123", passw:0, salt:0, phone:"911"}){
-     try{
-        await new Promise((resolve, reject)=> {
-            this.#bdPool.getConnection((err, connection)=>{
-               if (err) {
-                   reject(err);
-               } else {
-                   connection.query('START TRANSACTION;',
-                       (err, rows, fields)=>{
-                           if (err) {
-                             
-                               reject(err)
-                           } else {
-                                
-                               resolve(true);
-                           }
-                   })
-               }
-            })
-        });
-        //firstly fill a user_mail table:
-        let generatedUserId = await new Promise((resolve, reject)=> {
-            this.#bdPool.getConnection((err, connection)=>{
-               if (err) {
-                   reject(err);
-               } else {
-                   connection.query('INSERT INTO user_mail ( email) VALUES (?)',
-                       [par.email], (err, insResult)=>{
-                           if (err) {
-                               if(err.errno === 1062){
-                                   err.alrEx = true;
-                               } else{
-                                   err.alrEx =false;
-                               }
-                               reject(err)
-                           } else {
-                                 // Release the connection back to the pool
-                               connection.release();
-                               resolve(insResult);
-                           }
-                   })
-               }
-            })
-        });
-        //get user_id that has recently been generated (in prev step)
-        generatedUserId =  generatedUserId.insertId;
-        
-        //seconly inserting into "users"
-         await new Promise((resolve, reject)=> {
-             this.#bdPool.getConnection((err, connection)=>{
-                if (err) {
-                    reject(err);
-                } else {
-                    connection.query('INSERT INTO users ( user_id, passw, picture, uname,  salt, phone) VALUES (?, ?, ?, ?, ?, ?)',
-                        [generatedUserId, par.password, par.picture, par.name, par.salt, par.phone], (err, rows, fields)=>{
-                            if (err) {
-                                if(err.errno === 1062){
-                                    err.alrEx = true;
-                                } else{
-                                    err.alrEx =false;
-                                }
-                                reject(err)
-                            } else {
-                                  // Release the connection back to the pool
-                                connection.release();
-                                resolve(rows);
-                            }
-                    })
-                }
-             })
-         });
-
-         await new Promise((resolve, reject)=> {
-            this.#bdPool.getConnection((err, connection)=>{
-               if (err) {
-                   reject(err);
-               } else {
-                   connection.query('COMMIT;',
-                       (err, rows, fields)=>{
-                           if (err) {
-                             
-                               reject(err)
-                           } else {
-                                
-                               resolve(true);
-                           }
-                   })
-               }
-            })
-        });
-        //error handling
-     }catch(e){
-        
-        return new Promise((resolve, reject)=> {
-            this.#bdPool.getConnection((err, connection)=>{
-               if (err) {
-                   reject(err);
-               } else {
-                   connection.query('ROLLBACK;',
-                       (err, rows, fields)=>{
-                        reject(e);
-                   })
-               }
-            })
-        });
-     }
-        
-    }
+   
      //OK!****************** tested
     async getUserByEmail (email) {
         let connection = await this.#bdPool.getConnection();
@@ -235,163 +134,6 @@ class MysqlLayer {
 
 
 
-   /************* */
-class StorageOfSessions {
-    #mysqlPool;
-    constructor(pool){
-        this.#mysqlPool = pool;
-    }
-
-    async isSessionExists({hi_p=BigInt(1), lo_p=BigInt(2)}){
-      
-      
-        return new Promise((resolve, reject) => {
-          
-               this.#mysqlPool.getConnection((err, connection)=>{
-                  if (err) {
-                      reject(err);
-                  } else {
-                      connection.query(`SELECT user_id FROM session WHERE hi_p=? AND lo_p=?;`,
-                      [hi_p, lo_p],
-                        (err, rows, fields)=>{
-                              if (err) {
-                                  reject(err)
-                              } else {
-                                    // Release the connection back to the pool
-                                  connection.release();
-                                  if(rows.length === 1) {
-                                   resolve(true);
-                                  } else {
-                                   resolve(false);
-                                  }
-                                  
-                              }
-                      })
-                  }
-               })
-           });
-       
-      }
-
-      async getSessionById({hi_p=BigInt(3), lo_p=BigInt(7)}){
-         
-        return new Promise((resolve, reject) => {
-            this.#mysqlPool.getConnection((err, connection)=>{
-                  if (err) {
-                      reject(err);
-                  } else {
-                      connection.query(`SELECT * FROM session WHERE hi_p=? AND lo_p=?;`,
-                      [hi_p, lo_p],
-                        (err, rows, fields)=>{
-                              if (err) {
-                                  reject(err)
-                              } else {
-                                    // Release the connection back to the pool
-                                  connection.release();
-                                  if(rows.length === 1) {
-                                   resolve(rows[0]);
-                                  } else {
-                                   resolve(false);
-                                  }
-                                  
-                              }
-                      })
-                  }
-               })
-           });
-       
-      }
-
-    async clearSessionWhenExists(user_id=BigInt(5)) {
-       
-        return new Promise((resolve, reject) => {
-            this.#mysqlPool.getConnection((err, connection)=>{
-               if (err) {
-                   reject(err);
-               } else {
-                   connection.query(`DELETE  FROM session WHERE user_id=?;`,
-                   [user_id],
-                     (err, rows, fields)=>{
-                           if (err) {
-                               reject(err)
-                           } else {
-                                 // Release the connection back to the pool
-                               connection.release();
-                               if(rows.length === 1) {
-                                resolve(true);
-                               } else {
-                                resolve(false);
-                               }
-                               
-                           }
-                   })
-               }
-            })
-        });
-    }
-
-    async createUserSession ({hi_p=BigInt(1), lo_p=BigInt(2), user_id=BigInt(3),expired=BigInt(4), priv_k, pub_k, last_d=BigInt(5)}) {
-      
-        return new Promise((resolve, reject) => {
-            this.#mysqlPool.getConnection((err, connection)=>{
-               if (err) {
-                   reject(err);
-               } else {
-                   connection.query(`INSERT INTO session (hi_p, lo_p, user_id, expired, priv_k, pub_k, last_d ) VALUES (?,?,?,?,?,?,?);`,
-                   [hi_p, lo_p, user_id, expired, priv_k, pub_k, last_d],
-                     (err, rows, fields)=>{
-                           if (err) {
-                            if(err.errno === 1062){
-                                err.sessEx = true;
-                            } if(err.errno == 1452){
-                                err.wrongUsr = true;
-                            }
-                               reject(err)
-                           } else {
-                                 // Release the connection back to the pool
-                               connection.release();
-                                resolve(rows);
-                               
-                           }
-                   })
-               }
-            })
-        });
-    } 
-
-
-    async updateSessionTimestamps ({hi_p=BigInt(1), lo_p=BigInt(2), expired=BigInt(3), last_d=BigInt(4)}) {
-       
-        return new Promise((resolve, reject) => {
-            this.#mysqlPool.getConnection((err, connection)=>{
-               if (err) {
-                   reject(err);
-               } else {
-                   connection.query(`UPDATE session SET last_d=?, expired=? WHERE hi_p=? AND lo_p=?;`,
-                   [last_d, expired, hi_p, lo_p],
-                     (err, rows, fields)=>{
-                           if (err) {
-                            if (err.errno === 1062) {
-                                err.sessEx = true;
-                            } if (err.errno == 1452) {
-                                err.wrongUsr = true;
-                            }
-                               reject(err)
-                           } else {
-                                 // Release the connection back to the pool
-                               connection.release();
-                                resolve(rows);
-                               
-                           }
-                   })
-               }
-            })
-        });
-    } 
-
-
-}
-
-module.exports ={ MysqlLayer, StorageOfSessions};
+module.exports ={ MysqlLayer};
 
   
